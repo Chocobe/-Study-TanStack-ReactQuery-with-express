b33600f35f254cdfc0b7d04d845b5630d01a101d
Miles Kim <kyw05171@gmail.com>
2025-08-04 01:10:10 +0900

    connect database

--- backend/src/database/dbPool.ts
+++ backend/src/database/dbPool.ts
@@ -1,10 +1,16 @@
 import mysql from 'mysql2/promise';
 
 const dbPool = mysql.createPool({
-  host: '',
-  port: 6606,
-  user: '',
-  password: '',
+  host: process.env.DB_HOST || 'http://localhost',
+  port: 3306,
+  user: 'root',
+  password: 'hello-mysql',
+  database: process.env.DB_NAME || 'default',
+});
+
+dbPool.on('connection', () => {
+  console.log('- Connected MySQL -');
+  console.log(`- Database: ${process.env.DB_NAME}`);
 });
 
 export default dbPool;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import dbPool from './database/dbPool';
 
 dotenv.config({
   path: process.env.NODE_ENV === 'production' ? '.env' : '.env.development',
@@ -16,7 +17,7 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('combined'));
 
-app.use('/api/v1/hello', (req, res) => {
+app.get('/api/v1/hello', (req, res) => {
   console.log('🐳 /hello 🐳');
 
   res.json({
@@ -24,13 +25,31 @@ app.use('/api/v1/hello', (req, res) => {
   });
 });
 
-app.use('/api/v1/world', (req, res) => {
+app.get('/api/v1/world', (req, res) => {
   console.log('🚀 /world');
 
   res.json({
     message: 'World~',
   });
 })
+
+app.post('/api/v1/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  console.log('email: ', email);
+  console.log('password: ', password);
+
+  // TODO: try ~ catch 적용하기
+
+  const data = await dbPool.query(
+    'SELECT * FROM users WHERE email = ? AND password = ?',
+    [email, password]
+  );
+
+  const user = (data[0] as Array<any>)[0];
+
+  res.json(user);
+});
 
 app.listen(process.env.PORT, () => {
   console.log('-------------------------------');
