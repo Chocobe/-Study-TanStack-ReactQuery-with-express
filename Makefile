07216b0ed3f8ccfc1f68d116a631bf0b4e095337
Miles Kim <kyw05171@gmail.com>
2025-08-04 10:53:30 +0900

    apply .env.development to docker compose for development

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 dev-compose-up:
 	docker compose \
 		-f docker/docker-compose.dev.yaml \
+		--env-file ./backend/.env.development \
+		--env-file ./frontend/.env.development \
 		up
 
 dev-compose-down:
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
 
+import { useEffect } from 'react';
 import { useRoutes } from 'react-router';
 
 import routes from './routes/routes';
 
 function App() {
   const element = useRoutes(routes);
+
+  useEffect(() => {
+    console.log('import.meta.env.VITE_API_BASE_URL: ', import.meta.env.VITE_API_BASE_URL);
+  }, []);
 
   return element;
 }
