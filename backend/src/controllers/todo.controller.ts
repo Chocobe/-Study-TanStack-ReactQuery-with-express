40eba406fd02db7e410e5e34d56a9125c20f7f4e
Miles Kim <kyw05171@gmail.com>
2025-08-05 17:07:09 +0900

    create postTodo controller

--- backend/src/controllers/todo.controller.ts
+++ backend/src/controllers/todo.controller.ts
@@ -14,3 +14,17 @@ export async function getTodos(req: Request, res: Response) {
 
   res.json(todos);
 }
+
+export async function postTodo(req: Request, res: Response) {
+  const userId = req.user?.id;
+  const { content } = req.body;
+
+  if (!userId) {
+    return res.status(401).json({
+      message: 'Unauthorized',
+    });
+  }
+
+  const todo = await todoModel.createTodo(content, userId);
+  res.status(201).json(todo);
+}
