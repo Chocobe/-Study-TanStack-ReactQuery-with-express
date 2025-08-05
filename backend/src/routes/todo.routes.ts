25d8586507e79a698a9bc0fc278e14463ae989f1
Miles Kim <kyw05171@gmail.com>
2025-08-05 17:13:59 +0900

    add postTodo controller to todoRouter

--- backend/src/routes/todo.routes.ts
+++ backend/src/routes/todo.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import { mockUser } from '../middlewares/mockUser.middleware';
-import { getTodos } from '../controllers/todo.controller';
+import { 
+  getTodos,
+  postTodo,
+} from '../controllers/todo.controller';
 
 const todoRouter: Router = Router();
 
 todoRouter.use(mockUser);
 
 todoRouter.get('/', getTodos);
+todoRouter.post('/', postTodo);
 
 export default todoRouter;
