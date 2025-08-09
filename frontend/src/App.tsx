f6e0519cee489861fdc27a0279e0d2e05edbf4c8
Miles Kim <kyw05171@gmail.com>
2025-08-09 14:41:08 +0900

    install shadcn-ui sonner component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useRoutes } from 'react-router';
 
+import { Toaster } from './components/shadcn-ui/sonner';
 import AppQueryClientProvider from './lib/tanstack-query/AppQueryClientProvider';
 import routes from './routes/routes';
 import useInitAuthStore from './stores/authStore/hooks/useInitAuthStore';
@@ -16,6 +17,7 @@ function App() {
   return (
     <AppQueryClientProvider>
       {element}
+      <Toaster />
     </AppQueryClientProvider>
   );
 }
---
+++ frontend/src/components/shadcn-ui/sonner.tsx
@@ -0,0 +1,33 @@
+import { useTheme } from 'next-themes';
+import { Toaster as Sonner } from 'sonner';
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = 'system' } = useTheme();
+
+  return (
+    <Sonner
+      theme={theme as ToasterProps['theme']}
+      className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg group-[.toaster]:whitespace-pre-line',
+          description: 'group-[.toast]:text-muted-foreground',
+          actionButton:
+            'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
+          cancelButton:
+            'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
+          success:
+            'group-[.toaster]:bg-sky-200 group-[.toaster]:border-sky-700 group-[.toaster]:text-blue-600',
+          error:
+            'group-[.toaster]:bg-red-200 group-[.toaster]:border-red-700 group-[.toaster]:text-red-600',
+        },
+      }}
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
